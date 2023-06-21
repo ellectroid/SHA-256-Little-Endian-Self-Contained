19e8f44ba622418a9d6a8d5eccb7fd8ca50569d3
Ilya <72581491+ellectroid@users.noreply.github.com>
2023-06-22 01:43:29 +0200

    Init Code Commit

---
+++ sha256.hpp
@@ -0,0 +1,12 @@
+#ifndef SHA256_HPP_
+#define SHA256_HPP_
+
+#include <cstdint>
+
+
+void SHA256(uint8_t *Output, uint8_t *InputData, uint32_t InputByteLength);
+
+
+
+
+#endif /* SHA256_HPP_ */
---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include "sha256.hpp"
+int main() {
+	uint8_t Data[64] = { 0 };
+	Data[0] = 'H';
+	Data[1] = 'e';
+	Data[2] = 'l';
+	Data[3] = 'l';
+	Data[4] = 'o';
+	Data[5] = ',';
+	Data[6] = ' ';
+	Data[7] = 'w';
+	Data[8] = 'o';
+	Data[9] = 'r';
+	Data[10] = 'l';
+	Data[11] = 'd';
+	Data[12] = '!';
+	uint8_t Data_SHA256[32];
+
+	/* Real hash of string 'Hello, world!' 13 characters long (no null termination) is: 0x315F5BDB */
+
+	SHA256(Data_SHA256, Data, 13);
+	return 0;
+}
